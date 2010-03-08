1bfc699009389918b13f5036585b2acf15c7d399
razerbeans <razebeans@gmail.com>
2010-03-08 10:32:16 -0600

    Altered copyright information to reflect current state. Also, websocket connection may only be made using a file on a local document rather than a document on the arduino.

--- SocketServer.h
+++ SocketServer.h
@@ -2,6 +2,8 @@
 
    Webduino, a simple Arduino web server
    Copyright 2009 Ben Combee, Ran Talbott
+   Websocketduino, a websocket implementation for webduino
+   Copyright 2010 Randall Brewer
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
